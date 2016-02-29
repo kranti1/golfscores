604c91244a8e4288f62d4c953b5f6d8495114935
kranti1 <skartik@gmail.com>
2016-02-28 22:56:33 -0800

    changed from read_csv to ExcelFile

--- ExtractData.py
+++ ExtractData.py
@@ -4,24 +4,33 @@ import pandas as pd
 
 # to run this file in the console do this.
 # execfile('/Users/kartiks/Documents/github/GolfScores/ExtractData.py')
-
+'''
 path = './Data'
 filelist = listdir(path)
 filelist = [path + '/' + f for f in filelist]
+'''
+
 df = pd.DataFrame()
 combined_df = pd.DataFrame()
 
 
-for fname in filelist:
-    if 'Store' not in fname: #TODO: deal with DS_Store file type in directory
-        df = pd.read_csv(fname)
-        df['FILENAME'] = fname
+#Opening using XL directly
+srcFile = 'Data2015.xlsm'
+xl = pd.ExcelFile(srcFile)
+xlSheets = xl.sheet_names
+
+
+for sheet in xlSheets:
+    if 'Schedule' not in sheet: #TODO: deal with DS_Store file type in directory
+        df = xl.parse(sheet)
+        df['SHEETNAME'] = sheet
         df.columns = [x.upper() for x in df.columns]
         frames = [combined_df, df]
         combined_df = pd.concat(frames)
 
 #Final Combined File to a CSV
-combined_df.to_csv('./output.csv')
+#combined_df.to_csv('./output.csv', encoding='ascii')
+combined_df.to_excel('./output.xls')
 
 #enable printing the entire dataframe
 def print_full(x):
