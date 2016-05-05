1b7590861bf1e95111a625304e2835708b8bf332
Kranti <kartiks@Kartiks-iMac.local>
2016-05-04 21:41:38 -0700

    hd5 file and started each column type conversion

--- ExtractData.py
+++ ExtractData.py
@@ -2,6 +2,8 @@
 from os import listdir
 import pandas as pd
 
+
+
 # to run this file in the console do this.
 # execfile('/Users/kartiks/Documents/github/GolfScores/ExtractData.py')
 # execfile('C:\docs\github\golfscores\ExtractData.py')
@@ -16,7 +18,7 @@ combined_df = pd.DataFrame()
 
 
 #Opening using XL directly
-srcFile = 'Data2015.xlsm'
+srcFile = 'Data.xlsm'
 xl = pd.ExcelFile(srcFile)
 xlSheets = xl.sheet_names
 
@@ -31,10 +33,52 @@ for sheet in xlSheets:
 
 # Convert string to Numeric
 combined_df = combined_df.replace({u'\u2010': '-'}, regex=True)
-combined_df = combined_df.convert_objects(convert_numeric=True)
+
 
 # remap series types
 combined_df['DATE'] = combined_df['DATE'].astype('datetime64[ns]')
+combined_df['LOW POINT'] = combined_df['LOW POINT'].astype('string')
+combined_df['SHEETNAME'] = combined_df['SHEETNAME'].astype('string')
+combined_df['SIDE'] = combined_df['SIDE'].astype('string')
+combined_df['SIDE TOT.'] = combined_df['SIDE TOT.'].astype('string')
+combined_df['CLUB'] = combined_df['CLUB'].astype('string')
+
+combined_df['ATTACK ANG.'] = pd.to_numeric(combined_df['ATTACK ANG.'], errors='coerce')
+combined_df['BALL SPEED'] = pd.to_numeric(combined_df['BALL SPEED'], errors='coerce')
+combined_df['CARRY'] = pd.to_numeric(combined_df['CARRY'], errors='coerce')
+combined_df['CLUB PATH'] = pd.to_numeric(combined_df['CLUB PATH'], errors='coerce')
+combined_df['CLUB SPEED'] = pd.to_numeric(combined_df['CLUB SPEED'], errors='coerce')
+combined_df['DYN. LOFT'] = pd.to_numeric(combined_df['DYN. LOFT'], errors='coerce')
+combined_df['FACE ANG.'] = pd.to_numeric(combined_df['FACE ANG.'], errors='coerce')
+combined_df['FACE TO PATH'] = pd.to_numeric(combined_df['FACE TO PATH'], errors='coerce')
+
+
+#combined_df[''] = pd.to_numeric(combined_df[''], errors='coerce')
+
+type_int = ('HEIGHT' , 'LAND. ANG.' ,'LAUNCH ANG.')
+type_other = ('CLUB' , 'SIDE')
+type_all = type_int + type_other
+
+
+for h in type_int:
+    combined_df[h] = pd.to_numeric(combined_df[h], errors='coerce')
+
+
+'''
+
+SMASH FAC.
+SPIN AXIS
+SPIN LOFT
+SPIN RATE
+STROKE NO
+SWING DIR.
+SWING PL.
+TOTAL
+'''
+
+
+#combined_df = combined_df.convert_objects(convert_numeric=True)
+
 
 combined_df.to_excel('./output.xls')
 
